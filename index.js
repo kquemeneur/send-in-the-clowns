ed1584a75f6a4d68fd3a7ca497977a64312b387a
Katell <katell.quemeneur@gmail.com>
2023-11-22 16:33:12 +0100

    docs: add comments

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// à la racine on renvoie vers index.html
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// lancement du serveur
 server.listen(8001, () => {
   console.log('listening on http://localhost:8001');
 });
 
+// A la connexion d'un client au serveur
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  // quand on reçoit une image traitée
   socket.on('stream', function (data) {
+    // on renvoit l'image au client
     socket.emit('stream', data);
   });
  
