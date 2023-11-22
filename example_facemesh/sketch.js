e05bc1de131d0bcc8c2d37dc96012b2983205ab0
Katell <katell.quemeneur@gmail.com>
2023-11-22 10:07:51 +0100

    feat: add ml5 facemesh example to try something with z axis

---
+++ example_facemesh/sketch.js
@@ -0,0 +1,54 @@
+let facemesh;
+let video;
+let predictions = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  facemesh = ml5.facemesh(video, modelReady);
+
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new predictions are made
+  facemesh.on("predict", results => {
+    predictions = results;
+  });
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  console.log("Model ready!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints
+  drawKeypoints();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const keypoints = predictions[i].scaledMesh;
+
+    // Draw facial keypoints.
+    for (let j = 0; j < keypoints.length; j += 1) {
+      const [x, y, z] = keypoints[j];
+      
+      if(j==320){
+        //console.log(keypoints[j][2])
+        console.log(keypoints[j])
+        stroke(255, 0, 0)
+        fill(255, 0, 0);
+        ellipse(x, y, -y/20, -y/20);
+      }
+     
+
+      
+    }
+  }
+}
